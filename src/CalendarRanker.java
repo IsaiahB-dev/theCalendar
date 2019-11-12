add036f4eec2ce4458b6ef2bed2e3faaacbed74b
Jose Garcia Becerra <jfb0009@uah.edu>
2019-11-11 21:00:44 -0600

    Removed uneeded class

--- src/CalendarRanker.java
+++ src/CalendarRanker.java
@@ -40,47 +40,6 @@ public class CalendarRanker {
 
 }
 
-public class aUserCalendar {
-    public ArrayList<aAvailableTime> availableTimes;
-    public double matchPercent; // percent of primary user's
-                                // available time that matches
-    public String userName;
-
-    public aUserCalendar(ArrayList<aAvailableTime> availableTimes,
-                         String userName) {
-        this.availableTimes=availableTimes;
-        this.userName = userName;
-        this.matchPercent = 0.0;
-    }
-
-    public double getMatchPercent() {
-        return matchPercent;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public ArrayList<aAvailableTime> getAvailableTimes() {
-        return availableTimes;
-    }
-
-    public void setMatchPercent(double matchPercent) {
-        this.matchPercent = matchPercent;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setAvailableTimes(ArrayList<aAvailableTime> availableTimes) {
-        this.availableTimes = availableTimes;
-    }
-    public void addAvailableTime(aAvailableTime atime) {
-        this.availableTimes.add(atime);
-    }
-}
-
 public class aAvailableTime {
     public int startTime;
     public int endTime;
