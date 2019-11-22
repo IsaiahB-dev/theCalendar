f1f2b62751a17def15333e589b0a4989e03b9d4a
Rich Hansen <rsh0015@uah.edu>
2019-11-21 21:11:22 -0600

    Updated MainWindowGUI and RankingsGUI

--- src/MainWindowGUI.java
+++ src/MainWindowGUI.java
@@ -1,16 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.Calendar;
 
 public class MainWindowGUI {
     MainWindowGUI(){
         JFrame frame = new JFrame("Calendar Scheduler");
-        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         frame.setSize(1000, 800);
+
+        JPanel buttonsP = new JPanel();
 
         JButton time = new JButton("Add Time");
         time.setVisible(true);
         time.setBounds(50, 30, 10, 10);
-        frame.add(time);
+        buttonsP.add(time);
+
+        frame.add(buttonsP, BorderLayout.PAGE_START);
+
+        RankingsGUI rankingP = new RankingsGUI();
+        frame.add(rankingP, BorderLayout.CENTER);
+        rankingP.setVisible(true);
+
+        frame.pack();
+        frame.setVisible(true);
+
+
     }
 
     public static void main(String args[]){
