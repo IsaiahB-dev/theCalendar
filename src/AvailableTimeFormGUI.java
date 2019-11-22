27a3cd4782f2ae268bcd6e60cbb86da6a0501268
Isaiah B <ib0006@uah.edu>
2019-11-21 20:44:16 -0600

    Added a .gitignore for unnecessary files

--- src/AvailableTimeFormGUI.java
+++ src/AvailableTimeFormGUI.java
@@ -5,13 +5,13 @@ import java.awt.event.*;
 public class AvailableTimeFormGUI extends JFrame {
     //Variables
     JFrame timeWindow;
-    JLabel startTimeLabel,endTimeLabel ;
-    JButton bSubmitStartTime,bSubmitEndTime;
+    JLabel startTimeLabel,endTimeLabel , dayLabel;
+    JButton bSubmit;
     JPanel panel;
-    JTextField startTextField,endTextField;
-        /* *
-       Empty method at the moment
-         */
+    JTextField startTextField,endTextField, dayTextField;
+    /* *
+   Empty method at the moment
+     */
     public void setCalendarTimes() {
     }
     //Constructor for AvailableTimeFormGUI
@@ -20,29 +20,31 @@ public class AvailableTimeFormGUI extends JFrame {
         timeWindow = new JFrame("Available Time Form");
 
         //Created labels for the start and endtime placeholders
-        startTimeLabel = new JLabel("Enter start Time");
-        endTimeLabel = new JLabel("Enter end Time");
-
+        startTimeLabel = new JLabel("Enter Start Time");
+        endTimeLabel = new JLabel("Enter End Time");
+        dayLabel = new JLabel("Enter Day (1-7)");
         //created temporary fields for a user to input their start and endtime.
+
         startTextField = new JTextField();
         endTextField = new JTextField();
+        dayTextField = new JTextField();
         //Panel created for the labels and buttons. Used GridLayout because the content looks better
-        panel = new JPanel(new GridLayout(2,2 , 2, 2));
+        panel = new JPanel(new GridLayout(5,5 , 2, 2));
         panel.setBorder(BorderFactory.createEmptyBorder(50, 10, 150, 10));
         add(panel, BorderLayout.CENTER);
         //created buttons for the start and end times, along with bounds
-        bSubmitStartTime = new JButton("Submit Start Time");
-        bSubmitStartTime.setBounds(200, 150, 90, 50);
-        bSubmitEndTime = new JButton("Submit End Time");
-        bSubmitEndTime.setBounds(200, 150, 90, 50);
+        bSubmit = new JButton("Submit Here");
+        bSubmit.setBounds(200, 150, 90, 50);
+
 
         //added the contents to the panel
         panel.add(startTimeLabel);
         panel.add(startTextField);
-        panel.add(bSubmitStartTime);
         panel.add(endTimeLabel);
         panel.add(endTextField);
-        panel.add(bSubmitEndTime);
+        panel.add(dayLabel);
+        panel.add (dayTextField);
+        panel.add(bSubmit);
 
         //added the panel to the frame.
         timeWindow.add(panel);
