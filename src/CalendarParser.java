e2aad90a28cca7b21c2c79cb3bad10ddae3c7f13
Cameron Howell <crh0043@uah.edu>
2019-11-25 21:42:46 -0600

    Added code to calendarParser, will finish tonight

--- src/CalendarParser.java
+++ src/CalendarParser.java
@@ -1,5 +1,18 @@
-public class CalendarParser {
-    CalendarParser(){
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class CalendarParser {
+    CalendarParser(File googleCalendarFile) throws FileNotFoundException {
+        File file = googleCalendarFile;
+        Scanner scanner = new Scanner(file);
+        AvailableTime availableTime;
+        List<AvailableTime> timeList = new ArrayList<AvailableTime>();
+
+        while(scanner.hasNextLine()){
+            
+        }
     }
 }
