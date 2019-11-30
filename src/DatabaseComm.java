82e5cb4cb481cb68593d834af66b922268faed74
Jose Garcia Becerra <jfb0009@uah.edu>
2019-11-29 18:21:30 -0600

    Added implementation for classes

--- src/DatabaseComm.java
+++ src/DatabaseComm.java
@@ -1,4 +1,52 @@
-public class DatabaseComm {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-    
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class DatabaseComm {
+    private List<User> users= new ArrayList<>();
+
+    /**
+     * This function parses through the file and creates and array list of user objects for validating or adding users
+     * @return
+     */
+    public List<User> getUsers() {
+        try {
+            File fXmlFile = new File("sampleUsers.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+        
+            // Gets a list of all the nodes labelled user in the file 
+            NodeList nList = doc.getElementsByTagName("User");
+        
+            // Iterates through all the user nodes in the file
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node nNode = nList.item(i); // Gets the current node out of the list
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+
+                    // This creates a user from the elements in the user node
+                    User person = new User(eElement.getElementsByTagName("userName").item(0).getTextContent(), eElement.getElementsByTagName("password").item(0).getTextContent(), Integer.parseInt(eElement.getAttribute("id")));
+                    
+                    // This adds a person object to the list of people
+                    users.add(person);
+                }
+            }
+            return users;
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,5 +1,96 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    User(){
-        
-    }
+        private String userName;
+        private String password;
+        private int id;
+
+        /**
+         * Constructor for a user class being created from saved data
+         * @param name: User name for this class
+         * @param pass: Password for current user
+         * @param identifier: The id for the user to be creaded
+         */
+        public User(String name, String pass, int identifier)
+        {
+            this.userName = name;
+            this.password = pass;
+            this.id = identifier;
+        }
+
+        /**
+         * Constructor for a user class that is being created through the user creation gui and the identifier will be calculated
+         * @param name: User name for the created user
+         * @param pass: User password for the created user
+         */
+        public User(String name, String pass)
+        {
+            this.userName = name;
+            this.password = pass;
+        }
+
+        /**
+         * Returns the username for comparisons
+         */
+        public String getUsername()
+        {
+            return userName;
+        }
+
+        /**
+         * Funcion to create a user it will just set the variable fields and then let the user in
+         * the database will be updated at the end
+         * @param name: This is the username that the user chooses
+         * @param pass: This is the users chosen password
+         */
+        public void createUser(String name, String pass)
+        {
+            userName = name;
+            password = pass;
+
+            // This sets the user id for a new user to be one more than the last identifier in the file
+            DatabaseComm data = new DatabaseComm();
+            List<User> people = data.getUsers();
+            this.id = people.size() + 1;
+
+            people.add(this);
+        }
+
+        /**
+         * Function to pass the arguments of a requested login to a private function 
+         * @param name: The name of the user to be logged in 
+         * @param pass: The password of user to be logged in
+         * @return: 0 if the login has failed, 1 if the login is succesful
+         */
+        public boolean login(String name, String pass)
+        {
+            return validate(name, pass);
+        }
+
+        /**
+         * Function to validate user against list of saved users
+         * @param name: Username to be checked
+         * @param pass: Password to be verified
+         * @return: 0 if the login failed, 1 if the login is succesful
+         */
+        private boolean validate(String name, String pass)
+        {
+            // Gets a list of users
+            DatabaseComm data = new DatabaseComm();
+            List<User> people = new ArrayList<>();
+            people = data.getUsers();
+
+            // Iterates through it and compares names to see if the name is in the list
+            for(int i = 0; i < people.size(); i++)
+            {
+                User currentP = people.get(i);
+
+                if (currentP.getUsername().equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 }
