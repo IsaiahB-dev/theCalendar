0930adde265bd827aafd5282b234e3470c5db73c
Cameron Howell <crh0043@uah.edu>
2019-12-02 22:18:20 -0600

    CalendarGUI stuff

--- src/CalendarGUI.java
+++ src/CalendarGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import javax.swing.plaf.basic.*;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 
 public class CalendarGUI {
     public JInternalFrame calendarIFrame;
@@ -14,12 +16,30 @@ public class CalendarGUI {
 
         calendar = new JTable(15, 6);
         calendar.setBounds( 100,200,100,100);
-
+        DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
         calendarPanel.add(calendar);
 
         calendarIFrame.add(calendarPanel);
         calendarBIFrame = (BasicInternalFrameUI) calendarIFrame.getUI();
         calendarBIFrame.setNorthPane(null);
         calendarIFrame.setVisible(true);
+    }
+}
+
+class CalendarTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
     }
 }
