ed11c48d4629e89d2fc79f855c02fe0893b753c1
Isaiah B <ib0006@uah.edu>
2019-12-03 18:47:26 -0600

    Reverted back to the original GUI for due to time constraints and Added conditonal statements for passing values once the button is clicked. Also deleted the unnecessary packages. Will clean up this file once I make my final decision on the GUI formatting.

--- src/AvailableTimeFormGUI.java
+++ src/AvailableTimeFormGUI.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.lang.reflect.Array;
-import java.util.*;
 
 public class AvailableTimeFormGUI extends JFrame implements ActionListener {
 
     //Variables
+   private JFrame timeWindow;
    private JMenuBar menuBar = new JMenuBar();
    private JPopupMenu dayMenu = new JPopupMenu();
    private JMenuItem day = new JMenuItem();
-   private JFrame timeWindow;
+
    private JLabel startTimeLabel,endTimeLabel , dayLabel;
    private JButton bSubmit;
    private JPanel panel1, panel2;
@@ -23,7 +22,7 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
     //Constructor for AvailableTimeFormGUI
     AvailableTimeFormGUI() {
 
-        day = new JMenuItem("Monday");
+      /*  day = new JMenuItem("Monday");
         dayMenu.add(day);
         day = new JMenuItem("Tuesday");
         dayMenu.add(day);
@@ -34,7 +33,7 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
         day = new JMenuItem("Friday");
         dayMenu.add(day);
         menuBar.add(dayMenu);
-
+        */
         //sets the frame and title of the frame's window
 
         timeWindow = new JFrame("Available Time Form (Military Time)");
@@ -42,15 +41,14 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
         timeWindow.setSize(500, 500);
 
         //Created labels for the start time, end time, and day placeholders
-        startTimeLabel = new JLabel("    Enter Start Time (Only the hour)");
-        endTimeLabel = new JLabel("    Enter End Time (Only the hour)");
+        startTimeLabel = new JLabel("    Enter Start Time between 8 - 20 (Only the hour)");
+        endTimeLabel = new JLabel("    Enter End Time between 8 - 20 (Only the hour)");
         dayLabel = new JLabel("    Enter Day (0-4)");
 
         //created temporary fields for a user to input their start time, end time, and day.
         startTextField = new JTextField();
         endTextField = new JTextField();
-
-       // dayTextField = new JTextField();
+        dayTextField = new JTextField();
         // Panel1 created for the labels and textfields and panel2 is created for the submit button.
         // Used GridLayout because the content looks better
         //Still trying to figure out how to center the the submit button on panel2
@@ -70,15 +68,15 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
         panel1.add(endTimeLabel);
         panel1.add(endTextField);
         panel1.add(dayLabel);
-      //  panel1.add (dayTextField);
-        panel2.add(dayMenu);
-        dayMenu.setVisible(true);
+        panel1.add (dayTextField);
+     //   panel2.add(dayMenu);
+       // dayMenu.setVisible(true);
         panel1.add(panel2);
         //panel.add(bSubmit);
         panel2.add(bSubmit);
         //added an actionListener for the button to have actions.
         bSubmit.addActionListener(this);
-        day.addActionListener(new ActionListener(){
+       /* day.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
             }
         });
@@ -88,7 +86,7 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
                     dayMenu.show(Me.getComponent(), Me.getX(), Me.getY());
                 }
             }
-        });
+        });*/
 
         //added the panel to the frame.
         timeWindow.add(panel1);
@@ -105,27 +103,44 @@ public class AvailableTimeFormGUI extends JFrame implements ActionListener {
         new AvailableTimeFormGUI();
     }
 
-    /*
-    Actions that will be performed once the submit button is clicked.
-     The values placed inside each of the JtextFields will be captured.
-    */
+    /**
+     *
+     * @param e Method that will be performed once the bSubmit button is clicked.
+     *      The values placed inside each of the JtextFields will be captured
+     *      and place into the setStartTime, setEndTime, setDay objects of the AvailableTime class.
+     */
     public void actionPerformed(ActionEvent e) {
         AvailableTime s = new AvailableTime();
-        if(e.getSource() == bSubmit) {
-            //try,catch block that catches error when user leaves a JtextField blank
-         try {
-             int start = Integer.parseInt(startTextField.getText());
-             s.setStartTime(start);
-             int end = Integer.parseInt(endTextField.getText());
-             s.setEndTime(end);
-            // int day = Integer.parseInt(dayTextField.getText());
-             //s.setDays(day);
+            if(e.getSource() == bSubmit) {
+               //try,catch block that catches error when user leaves a JtextField blank
+            try {
+                int start = Integer.parseInt(startTextField.getText());
+                if(start > 7 && start < 21) {
+                    s.setStartTime(start);
+                }
+               else {
+                    JOptionPane.showMessageDialog(timeWindow, "You input the wrong value for your start time, try again.");
+                }
+                int end = Integer.parseInt(endTextField.getText());
+               if(end < 21) {
+                   s.setEndTime(end);
+               }
+               else {
+                   JOptionPane.showMessageDialog(timeWindow, "You input the wrong value for your end time, try again.");
 
-         }
-         catch (NumberFormatException ex) {
-             System.out.print("Exception caught here");
-         }
-            JOptionPane.showMessageDialog(timeWindow, "Your available time has been updated.");
+               }
+               int day = Integer.parseInt(dayTextField.getText());
+               if(day < 5) {
+                   s.setDay(day);
+               }
+               else {
+                   JOptionPane.showMessageDialog(timeWindow, "You input the wrong value for your day, try again.");
+               }
+            }
+            catch (NumberFormatException ex) {
+                System.out.print("Exception caught here");
+            }
+            JOptionPane.showMessageDialog(timeWindow, "Your available time has been updated only for fields input correctly.");
         }
     }
 
