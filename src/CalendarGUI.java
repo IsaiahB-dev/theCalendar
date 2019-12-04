d08a335731c3efc42b6d0beb0a88c9629f4e84fa
Cameron Howell <crh0043@uah.edu>
2019-12-04 04:06:04 -0600

    Fixed bad array index

--- src/CalendarGUI.java
+++ src/CalendarGUI.java
@@ -12,7 +12,7 @@ public class CalendarGUI {
         calendarPanel.setLayout(new GridLayout(14, 6));
         calendarPanel.setVisible(true);
 
-        Border border = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1);
+        Border border = BorderFactory.createLineBorder(Color.GRAY, 1);
 
         headers = new JLabel[6];
         headers[0] = new JLabel("Time", SwingConstants.CENTER);
@@ -24,14 +24,22 @@ public class CalendarGUI {
 
         for (int i = 0; i < headers.length; i++){
             headers[i].setBorder(border);
+            headers[i].setOpaque(true);
+            headers[i].setBackground(Color.LIGHT_GRAY);
             calendarPanel.add(headers[i]);
         }
-        calendar = new JLabel[13][5];
+        calendar = new JLabel[13][6];
         for (int i = 0; i < 13; i++){
             for (int j = 0; j < 6; j++){
                 calendar[i][j] = new JLabel("");
+                calendar[i][j].setBorder(border);
                 calendarPanel.add(calendar[i][j]);
             }
+        }
+        for (int i = 0; i < 6; i++){
+            calendar[i][0].setText((i + 7) + " AM");
+            calendar[i][0].setHorizontalAlignment(JLabel.CENTER);
+            calendar[i][0].setVerticalAlignment(JLabel.CENTER);
         }
     }
 }
