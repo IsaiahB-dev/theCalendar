2caf8dcd4006e02c2ee204b9eea45df3343883b8
Cameron Howell <crh0043@uah.edu>
2019-12-03 20:11:35 -0600

    Touch-ups

--- src/MainWindowGUI.java
+++ src/MainWindowGUI.java
@@ -24,7 +24,7 @@ public class MainWindowGUI {
         RankingsGUI r = new RankingsGUI();
         CalendarGUI c = new CalendarGUI();
         frame.add(r.rankingIFrame, BorderLayout.EAST);
-        frame.add(c.calendarIFrame, BorderLayout.CENTER );
+        frame.add(c.calendarPanel, BorderLayout.CENTER );
         frame.setSize(1000, 800);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
