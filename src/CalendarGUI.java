ee3d94bf6f0d89049198bb7f03236244f779a4a3
Cameron Howell <crh0043@uah.edu>
2019-12-04 03:53:08 -0600

    Almost there.

--- src/CalendarGUI.java
+++ src/CalendarGUI.java
@@ -1,27 +1,37 @@
 import javax.swing.*;
-import javax.swing.plaf.basic.*;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableCellRenderer;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+
 
 public class CalendarGUI {
-    public JTable calendar;
     public JPanel calendarPanel;
-    public JScrollPane scrollPane;
-    public CalendarTableModel model;
-    CalendarGUI(List<CalendarTime> CalTimes){
+    public JLabel[][] calendar;
+    public JLabel[] headers;
+    CalendarGUI() {
         calendarPanel = new JPanel();
-        calendarPanel.setLayout(new BorderLayout());
+        calendarPanel.setLayout(new GridLayout(14, 6));
         calendarPanel.setVisible(true);
-        model = new CalendarTableModel(CalTimes);
-        calendar = new JTable(model);
-        calendar.setDefaultRenderer(CalendarTime.Times.class, new CalendarRenderer());
-        calendar.setRowHeight(35);
-        scrollPane = new JScrollPane();
-        scrollPane.setViewportView(calendar);
-        calendarPanel.add(scrollPane, BorderLayout.CENTER);
+
+        Border border = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 1);
+
+        headers = new JLabel[6];
+        headers[0] = new JLabel("Time", SwingConstants.CENTER);
+        headers[1] = new JLabel("Monday", SwingConstants.CENTER);
+        headers[2] = new JLabel("Tuesday", SwingConstants.CENTER);
+        headers[3] = new JLabel("Wednesday", SwingConstants.CENTER);
+        headers[4] = new JLabel("Thursday", SwingConstants.CENTER);
+        headers[5] = new JLabel("Friday", SwingConstants.CENTER);
+
+        for (int i = 0; i < headers.length; i++){
+            headers[i].setBorder(border);
+            calendarPanel.add(headers[i]);
+        }
+        calendar = new JLabel[13][5];
+        for (int i = 0; i < 13; i++){
+            for (int j = 0; j < 6; j++){
+                calendar[i][j] = new JLabel("");
+                calendarPanel.add(calendar[i][j]);
+            }
+        }
     }
 }
--- src/MainWindowGUI.java
+++ src/MainWindowGUI.java
@@ -6,42 +6,14 @@ import java.util.List;
 import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
 
-public class MainWindowGUI implements ActionListener{
+public class MainWindowGUI implements ActionListener {
     JFrame frame;
     JPanel buttonsP;
     JButton time;
     User user;
-    List<CalendarTime> CalTimes;
-    CalendarTime r1 = new CalendarTime(8, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r2 = new CalendarTime(9, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r3 = new CalendarTime(10, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r4 = new CalendarTime(11, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r5 = new CalendarTime(12, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r6 = new CalendarTime(1, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r7 = new CalendarTime(2, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r8 = new CalendarTime(3, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r9 = new CalendarTime(4, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r10 = new CalendarTime(5, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r11 = new CalendarTime(6, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r12 = new CalendarTime(7, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
-    CalendarTime r13 = new CalendarTime(8, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N, CalendarTime.Times.N);
+
     MainWindowGUI(User user) {
         this.user = user;
-
-        CalTimes = new ArrayList<>();
-        CalTimes.add(r1);
-        CalTimes.add(r2);
-        CalTimes.add(r3);
-        CalTimes.add(r4);
-        CalTimes.add(r5);
-        CalTimes.add(r6);
-        CalTimes.add(r7);
-        CalTimes.add(r8);
-        CalTimes.add(r9);
-        CalTimes.add(r10);
-        CalTimes.add(r11);
-        CalTimes.add(r12);
-        CalTimes.add(r13);
 
         // New database comm object to get and save data
         DatabaseComm stream = new DatabaseComm();
@@ -51,24 +23,7 @@ public class MainWindowGUI implements ActionListener{
         if (user.getId() <= calendars.size()) {
             UserCalendar cal = calendars.get(user.getId() - 1); // Pulls out the calendar to save the time to
             java.util.List<AvailableTime> times = cal.getAvailableTimes(); // Gets a list of that calendars times
-            for (AvailableTime t : times){
-                if(t.getDay() == 1){
-                    for (CalendarTime c : CalTimes){
-                        if(CalTimes.indexOf(c) <= (t.getStartTime() - 8) && CalTimes.indexOf(c) >= (t.getEndTime() - 8) ){
-                            c.setMonday(CalendarTime.Times.Y);
-                        }
-                    }
-                }
-            }
         }
-        else { // If his id is larger then he is new and needs to be added
-            UserCalendar newCalendar = new UserCalendar(); // Creates a new calendar object for the new user
-            newCalendar.setId(user.getId()); // Sets the id to the new users id
-            // Get a list of available times from the new user calendar object
-            List<AvailableTime> times = newCalendar.getAvailableTimes();
-            calendars.add(newCalendar); // Add the new calendar to the list
-        }
-
 
         frame = new JFrame("Calendar Scheduler");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -85,8 +40,8 @@ public class MainWindowGUI implements ActionListener{
 
         time.addActionListener(this);
 
-        RankingsGUI r = new RankingsGUI();
-        CalendarGUI c = new CalendarGUI(CalTimes);
+        RankingsGUI r = new RankingsGUI(user);
+        CalendarGUI c = new CalendarGUI();
         frame.add(r.rankingP, BorderLayout.EAST);
         frame.add(c.calendarPanel, BorderLayout.CENTER);
         frame.setSize(1000, 800);
@@ -106,6 +61,7 @@ public class MainWindowGUI implements ActionListener{
 
     /**
      * This is the main function to initialize the software
+     *
      * @param args
      */
     public static void main(String[] args) {
